5d79158ca663c70b10e9db262f57118d5f85a054
WMXPY <wm@mengw.io>
2020-09-02 10:43:39 -0500

    Revert "auto fix lint"
    This reverts commit d6d2ddbfb66ea1b1aaedf5db87b7588b955755e2.

--- src/declare.ts
+++ src/declare.ts
@@ -1,3 +1,15 @@
+/**
+ * @author WMXPY
+ * @namespace Definition
+ * @description Declare
+ */
+
+export enum DEFAULT_BRONTOSAURUS_NAMESPACE {
+
+    DEFAULT = "brontosaurus.default",
+    ADMIN = "brontosaurus.admin",
+}
+
 export type Basics = string | number | boolean;
 export type Encryptable = Basics | Basics[] | Record<string, Basics> | null | undefined;
 
